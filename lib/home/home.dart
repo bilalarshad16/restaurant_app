65f727c70f881eca2e1e5c5a7b58ef19d51e19b9
Bilal Arshad <bilalarshad6780@gmail.com>
2025-01-31 17:44:20 +0500

    friday's work

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -4,7 +4,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 5, // Number of tabs, adjust as needed
+      length: 5, // Number of tabs
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
@@ -12,21 +12,18 @@ class Home extends StatelessWidget {
           leading: Padding(
             padding: const EdgeInsets.all(8.0),
             child: IconButton(
-              icon: Icon(Icons.menu, color: Colors.black, size: 30),
-              onPressed: () {
-                // Add functionality here
-              },
+              icon: const Icon(Icons.menu, color: Colors.black, size: 30),
+              onPressed: () {},
             ),
           ),
           actions: [
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: IconButton(
-                icon:
-                    Icon(Icons.shopping_cart, color: Colors.black, size: 30.0),
+                icon: const Icon(Icons.shopping_cart,
+                    color: Colors.black, size: 30),
                 onPressed: () {
-                  Navigator.pushNamed(
-                      context, '/orders'); // Assuming you will define routes
+                  Navigator.pushNamed(context, '/orders');
                 },
               ),
             ),
@@ -37,32 +34,26 @@ class Home extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 10.0),
+              const Padding(
+                padding: EdgeInsets.only(left: 10.0),
                 child: Text(
                   'Delicious',
-                  style: TextStyle(
-                    fontSize: 38,
-                    fontWeight: FontWeight.bold,
-                  ),
+                  style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10.0),
+              const Padding(
+                padding: EdgeInsets.only(left: 10.0),
                 child: Text(
                   'food for you',
-                  style: TextStyle(
-                    fontSize: 38,
-                    fontWeight: FontWeight.bold,
-                  ),
+                  style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold),
                 ),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               TextField(
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: Colors.grey[200],
-                  prefixIcon: Icon(Icons.search),
+                  prefixIcon: const Icon(Icons.search),
                   hintText: 'Search',
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(30),
@@ -70,30 +61,39 @@ class Home extends StatelessWidget {
                   ),
                 ),
               ),
-              SizedBox(height: 20),
-              TabBar(
-                isScrollable: true, // Enables horizontal scrolling
-                indicatorColor: Colors.orange,
-                labelColor: Colors.orange,
-                unselectedLabelColor: Colors.black,
-                tabs: [
-                  Tab(text: 'Foods'),
-                  Tab(text: 'Drinks'),
-                  Tab(text: 'Snacks'),
-                  Tab(text: 'Sauces'),
-                  Tab(text: 'Rice'),
-                  // Add more tabs as needed
-                ],
+              const SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.orange[900],
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    topRight: Radius.circular(12),
+                    bottomLeft: Radius.circular(8),
+                  ),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: const TabBar(
+                  isScrollable: true,
+                  indicatorColor: Colors.white,
+                  labelColor: Colors.white,
+                  unselectedLabelColor: Colors.white70,
+                  tabs: [
+                    Tab(text: 'Foods'),
+                    Tab(text: 'Drinks'),
+                    Tab(text: 'Snacks'),
+                    Tab(text: 'Sauces'),
+                    Tab(text: 'Rice'),
+                  ],
+                ),
               ),
               Expanded(
                 child: TabBarView(
                   children: [
-                    Center(child: Text('Foods Content')),
-                    Center(child: Text('Drinks Content')),
-                    Center(child: Text('Snacks Content')),
-                    Center(child: Text('Sauces Content')),
-                    Center(child: Text('Rice Content')),
-                    // Add corresponding content for more tabs
+                    MenuList(), // Foods Tab
+                    MenuList(), // Drinks Tab
+                    MenuList(), // Snacks Tab
+                    MenuList(), // Sauces Tab
+                    MenuList(), // Rice Tab
                   ],
                 ),
               ),
@@ -104,34 +104,84 @@ class Home extends StatelessWidget {
           backgroundColor: Colors.orange[900],
           fixedColor: Colors.white,
           type: BottomNavigationBarType.fixed,
-          items: [
+          items: const [
             BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home,
-                size: 30.0,
-              ),
-              label: 'Home',
-            ),
+                icon: Icon(Icons.home, size: 30.0), label: 'Home'),
             BottomNavigationBarItem(
-              icon: Icon(Icons.favorite, size: 30.0),
-              label: 'Favorites',
-            ),
+                icon: Icon(Icons.favorite, size: 30.0), label: 'Favorites'),
             BottomNavigationBarItem(
-              icon: Icon(
-                Icons.person,
-                size: 30.0,
-              ),
-              label: 'Account',
-            ),
+                icon: Icon(Icons.person, size: 30.0), label: 'Account'),
             BottomNavigationBarItem(
-              icon: Icon(
-                Icons.settings,
-                size: 30.0,
-              ),
-              label: 'Settings',
-            ),
+                icon: Icon(Icons.settings, size: 30.0), label: 'Settings'),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MenuList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.all(16),
+      itemCount: 4, // Number of times to show the MenuCard
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 16.0),
+          child: MenuCard(
+            title: 'Veggie Tomato Mix',
+            price: 'N1,900',
+            imagePath: 'assets/veggie.jpg', // Local asset image
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MenuCard extends StatelessWidget {
+  final String title;
+  final String price;
+  final String imagePath; // Changed from imageUrl
+
+  const MenuCard(
+      {required this.title, required this.price, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      elevation: 4,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  price,
+                  style: const TextStyle(fontSize: 16, color: Colors.orange),
+                ),
+              ],
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(16),
+              bottomRight: Radius.circular(16),
+            ),
+            child: Image.asset(imagePath,
+                fit: BoxFit.cover, height: 150, width: double.infinity),
+          ),
+        ],
       ),
     );
   }
